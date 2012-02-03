a9a463be00f76924501cf2de442bc99d54ab96b9
Roy Shilkrot <roy.shil@gmail.com>
2012-02-03 01:05:32 -0500

    Factory.cpp needs to be platform specific

---
+++ Win/Factory.cpp
@@ -0,0 +1,55 @@
+/**********************************************************\ 
+ 
+ Auto-generated Factory.cpp
+ 
+ This file contains the auto-generated factory methods 
+ for the kinectathome project
+ 
+\**********************************************************/
+
+#include "FactoryBase.h"
+#include "kinectathome.h"
+#include <boost/make_shared.hpp>
+
+class PluginFactory : public FB::FactoryBase
+{
+public:
+    ///////////////////////////////////////////////////////////////////////////////
+    /// @fn FB::PluginCorePtr createPlugin(const std::string& mimetype)
+    ///
+    /// @brief  Creates a plugin object matching the provided mimetype
+    ///         If mimetype is empty, returns the default plugin
+    ///////////////////////////////////////////////////////////////////////////////
+    FB::PluginCorePtr createPlugin(const std::string& mimetype)
+    {
+        return boost::make_shared<kinectathome>();
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    /// @see FB::FactoryBase::globalPluginInitialize
+    ///////////////////////////////////////////////////////////////////////////////
+    void globalPluginInitialize()
+    {
+        kinectathome::StaticInitialize();
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    /// @see FB::FactoryBase::globalPluginDeinitialize
+    ///////////////////////////////////////////////////////////////////////////////
+    void globalPluginDeinitialize()
+    {
+        kinectathome::StaticDeinitialize();
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// @fn getFactoryInstance()
+///
+/// @brief  Returns the factory instance for this plugin module
+///////////////////////////////////////////////////////////////////////////////
+FB::FactoryBasePtr getFactoryInstance()
+{
+    static boost::shared_ptr<PluginFactory> factory = boost::make_shared<PluginFactory>();
+    return factory;
+}
+
